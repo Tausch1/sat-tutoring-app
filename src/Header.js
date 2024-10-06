9ea9068346ce465b9dbc84fd522aa5d202175e3a
Matt Tausch <matthewtausch@gmail.com>
2024-10-06 19:52:03 -0400

    Initial commit: Basic React app setup

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>SAT Tutoring App</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Practice</li>
+          <li>Progress</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
